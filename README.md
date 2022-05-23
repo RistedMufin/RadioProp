2dc64bf613289008e9fa12ee89bb34281781f573
RistedMufin <106056537+RistedMufin@users.noreply.github.com>
2022-05-23 08:55:13 +0200

    Update and rename README.md to fxmanifest.lua

--- README.md
+++
@@ -1,3 +0,0 @@
-Hej og velkomen til min Github er der problemer med mine scripts er du mere end velkomen til at kontakte mig på discord Risted Æbleskive#8203
-jeg er interesseret i Fivem Kodning
-Jeg er ved at lære at kode i Java
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+games {'gta5'}
+
+author 'Radirop - Risted Æbleskive'
+description 'Radio prop der viser når du trykker LALT - Made by Risted Æbleskive'
+version '2.0'
+
+
+client_scripts {
+	'radio.lua'
+}
